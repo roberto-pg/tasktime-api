f3263105426c35b343e9858f1703956b445727a1
Roberto <rpg61@hotmail.com>
2020-10-17 23:47:04 -0300

    Atualiza o Mongoose e a conexao com o MongoDB

--- src/app.js
+++ src/app.js
@@ -10,17 +10,17 @@ require('dotenv/config');
 const userRoutes = require('../src/app/controllers/user');
 const noteRoutes = require('../src/app/controllers/noteController');
 
-var mongoDB = 'mongodb://172.30.0.3/dbnotes';
+var mongoDB = `mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`;
 mongoose.connect(mongoDB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    user: process.env.DB_USER,
-    pass: process.env.DB_PASS,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS,
 }).then(() => {
-    console.log('Successfully connected to the MongoDb');
+  console.log('Successfully connected to the MongoDb');
 }).catch(err => {
-    console.log('Error connecting to the MongoDb');
-    process.exit();
+  console.log('Error connecting to the MongoDb');
+  process.exit();
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,14 +28,14 @@ app.use(bodyParser.json());
 
 // Cors:
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
 });
 
 // Routes wich should handle requests
